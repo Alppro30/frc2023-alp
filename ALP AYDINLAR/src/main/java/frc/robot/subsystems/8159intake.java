951c37a4d51d61c9a93b89b4f08742f7537e3ebe
Alppro30 <112391538+Alppro30@users.noreply.github.com>
2023-07-28 13:05:32 +0300

    Update 8159intake.java

--- ALP AYDINLAR/src/main/java/frc/robot/subsystems/8159intake.java
+++ ALP AYDINLAR/src/main/java/frc/robot/subsystems/8159intake.java
@@ -1,21 +1,29 @@
-public class 8159intake {
-    import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-public class 8159Intake extends SubsystemBase {
-    private final SpeedController intakeMotor;
+package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.Subsystembase; 
 
-    public 8159Intake(SpeedController intakeMotor) {
-        this.intakeMotor = intakeMotor;
-    }
+public class IntakeSubsystem extends SubsystemBase {
 
-    public void runIntake(double speed) {
-        intakeMotor.set(speed);
-    }
+private final TalconX intakerightMotor = new TalconX(3);
+private final TalconX intakeleftMotor = new TalconX(4);
 
-    public void stopIntake() {
-        intakeMotor.stopMotor();
-    }
+       public IntakeSubsystem() {
+      } 
+  public void setPosition(boolean open ) {
+      if (open) {
+    intakeleftMotor.set(1);
+    intakerightMotor.set(1);
+  } else {
+    intakerightMotor.set(-1);
+    intakeleftMotor.set(-1);
+  }
+  
+  }
 }
 
-}
+// CODED BY ALP WITH ♡
